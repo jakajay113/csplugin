b275908c2b646e2d5e1cf729fb1f8203dbae02f7
Jaka Jay S. Sitjar <79232538+jakajay113@users.noreply.github.com>
2025-05-16 09:58:53 +0800

    Update csnewclient-silay.js

--- csnewclient-silay.js
+++ csnewclient-silay.js
@@ -35,7 +35,7 @@ function csplugindist() {
                                 </div>
                                 <div class="chat-partner-name">
                                     <span class="status online"></span>
-                                    <a href="#">Chat Support - Online Payment</a>
+                                    <a href="#">Chat</a>
                                 </div>
                                 <div class="chatbox-icons">
                                     <a href="javascript:void(0);"><i class="fa fa-minus"></i></a>
@@ -43,11 +43,11 @@ function csplugindist() {
                             </div>
     
                             <div class="chat-messages">
-                                <div class="message-box-holder"><div class="bot-box">Hi, how can I help you?</div></div>
-                                <div class="message-box-holder bot-question"><div class="bot-box">How can I apply for a new business online?</div></div>
-                                <div class="message-box-holder bot-question"><div class="bot-box">How can I renew my business online?</div></div>
-                                <div class="message-box-holder bot-question"><div class="bot-box">How can I pay my real property tax due?</div></div>
-                                <div class="message-box-holder bot-question"><div class="bot-box">Chat with Agent?</div></div>
+                                // <div class="message-box-holder"><div class="bot-box">Hi, how can I help you?</div></div>
+                                // <div class="message-box-holder bot-question"><div class="bot-box">How can I apply for a new business online?</div></div>
+                                // <div class="message-box-holder bot-question"><div class="bot-box">How can I renew my business online?</div></div>
+                                // <div class="message-box-holder bot-question"><div class="bot-box">How can I pay my real property tax due?</div></div>
+                                // <div class="message-box-holder bot-question"><div class="bot-box">Chat with Agent?</div></div>
                             </div>
     
                             <div class="chat-input-holder">
