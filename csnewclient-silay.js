83e82faf696fff82bb6e5871555f5e07c824aa29
Jaka Jay S. Sitjar <79232538+jakajay113@users.noreply.github.com>
2025-05-16 12:32:28 +0800

    Update csnewclient-silay.js

--- csnewclient-silay.js
+++ csnewclient-silay.js
@@ -43,7 +43,8 @@ function csplugindist() {
                             </div>
     
                             <div class="chat-messages">
-                                
+                               
+                                <div class="message-box-holder bot-question"><div class="bot-box">Chat with Agent?</div></div>
                             </div>
     
                             <div class="chat-input-holder">
